3884f63ef5857ab91846857bd81b2730717c974f
Sma3in96 <oumouss.ismail5@gmail.com>
2023-09-07 19:27:00 +0100

    task1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from calculator_1.py import add, sub, mul, div
+
+
+def cal_add(a, b):
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+
+def cal_sub(a, b):
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+
+
+def cal_mul(a, b):
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+
+
+def cal_div(a, b):
+    print("{} + {} = {}".format(a, b, div(a, b)))
+
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    cal_add(a, b)
+    cal_sub(a, b)
+    cal_mul(a, b)
+    cal_div(a, b)
