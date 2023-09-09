a5cfc04ae1046d6f528bd86dc007d0c6ae402973
Sma3in96 <oumouss.ismail5@gmail.com>
2023-09-09 16:24:53 +0100

    task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
+    if matrix == [[]]:
         print(end='\n')
     for i in range(0, len(matrix)):
-        if matrix[i] is None:
+        if matrix[i] == []:
             print(end='\n')
         for j in range(0, len(matrix[i])):
             if j == len(matrix[i]) - 1:
