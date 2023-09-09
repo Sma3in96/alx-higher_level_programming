87e696815076dc8a9bf6feeb817cbcd34e7a6bf8
Sma3in96 <oumouss.ismail5@gmail.com>
2023-09-09 16:17:33 +0100

    task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return None
     for i in range(0, len(matrix)):
+        if matrix[i] is None:
+            return None
         for j in range(0, len(matrix[i])):
             if j == len(matrix[i]) - 1:
                 print("{:d}".format(matrix[i][j]))
