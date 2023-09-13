ccbf82e8f9a446b6a02b8e95ce0904550b1227f5
Sma3in96 <oumouss.ismail5@gmail.com>
2023-09-13 20:09:05 +0100

    task0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def square_number(a):
-    return a**2
+    return a * a
 
 
 def square_matrix_simple(matrix=[]):
-    temp = [[]]
+    temp = matrix.copy()
     for i in range(0, len(matrix)):
         temp[i] = list(map(square_number, matrix[i]))
     return temp
