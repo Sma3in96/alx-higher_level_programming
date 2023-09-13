342bea47e389fe300aaca3ff29b2ae5ecf1abad0
Sma3in96 <oumouss.ismail5@gmail.com>
2023-09-13 23:18:10 +0100

    task9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    for key in a_dictionary:
+        new_dict[key] = map(lambda x: x * 2, a_dictionary[key])
