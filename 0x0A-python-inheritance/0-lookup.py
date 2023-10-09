0ea2ff530beac3d80dce182d34044ddf3b60f496
Sma3in96 <oumouss.ismail5@gmail.com>
2023-10-09 12:42:52 +0100

    task

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,11 @@
 #!usr/bin/python3
 def lookup(obj):
-    """this function return all attrs and methods of a class"""
+    """
+    this function return all attrs and methods of a class
+    args:
+    obj = class, object or module
+    return:
+    all attrs and methods
+    """
 
     return (dir(obj))
