2a0237f786254697c5ae01125f43c5564a5a5200
Sma3in96 <oumouss.ismail5@gmail.com>
2023-12-11 15:29:13 +0100

    task2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 if (args[0] && args[1]) {
   console.log('Arguments found');
-} else if (args[1]) {
+} else if (args[0]) {
   console.log('Argument found');
 } else {
   console.log('No argument');
