30ebe4dfff74b6facba07395b33b19d4f1faa83f
Sma3in96 <oumouss.ismail5@gmail.com>
2023-12-11 22:29:41 +0100

    task4&5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const list = process.argv.slice(2);
 if (list[0]) {
-  let x = parseInt(list[0]);
+  const x = parseInt(list[0]);
   if (x) {
-    console.log(x);
+    console.log('My number:', x);
   } else {
     console.log('Not a number');
   }
