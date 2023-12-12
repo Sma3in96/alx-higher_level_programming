d77f5e9884dc493873f5b218985d9d793a29f27f
Sma3in96 <oumouss.ismail5@gmail.com>
2023-12-12 02:47:10 +0100

    last commit

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-exports.callMeMoby =  function (n, b) {
+exports.callMeMoby = function (n, b) {
   for (let i = 0; i < n; i++) {
     b();
   }
-}
+};
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -2,4 +2,4 @@
 exports.addMeMaybe = function (n, func) {
   n = parseInt(n) + 1;
   func(n);
-}
+};
