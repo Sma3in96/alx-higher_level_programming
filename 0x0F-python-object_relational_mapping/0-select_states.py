dfa1222a24d19a592712373af544ec627598ec25
Sma3in96 <oumouss.ismail@gmail.com>
2023-12-14 18:39:14 +0100

    testing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,42 +1,37 @@
 #!/usr/bin/python3
 """ select states"""
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
-#from slqalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
-
+# Construct the database URL
 url_db = f"mysql+mysqlconnector://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
 
+# Create the SQLAlchemy engine
 engine = create_engine(url_db, echo=True)
 
+# Create a metadata object
 metadata = MetaData()
 
+# Reflect the existing 'states' table from the database
+states_table = Table('states', metadata, autoload_with=engine)
 
-states_table = Table('states', metadata,
-                     Column('id', Integer, primary_key=True),
-                     Column('name', String(256)),
-                     )
-
-
-metadata.reflect(bind=engine)
-
-
-class States:
+# Create a class for the existing table
+class State:
     pass
 
-
+# Map the class to the existing table
 mapper = Table.mapper_for(states_table)
-mapper(States)
+mapper(State)
 
-
+# Create a session
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# Query data from the 'states' table using the State class
+states = session.query(State).all()
+for state in states:
+    print(f"State ID: {state.id}, Name: {state.name}")
 
-states = session.query(States).all()
-for States in states:
-    print(f"States ID: {States.id}, Name: {States.name}")
-
-
+# Close the session
 session.close()
