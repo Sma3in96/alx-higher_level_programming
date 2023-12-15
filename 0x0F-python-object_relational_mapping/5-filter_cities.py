d9bebc0dce314f6d0d98d825b4cdebae13e772eb
Sma3in96 <oumouss.ismail@gmail.com>
2023-12-15 13:11:17 +0100

    task 2

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,6 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
-
     cur = db.cursor()
     cur.execute("SELECT cities.name \
     FROM cities JOIN states ON cities.states_id = states.id \
