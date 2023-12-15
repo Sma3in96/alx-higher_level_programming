c92951145a3507cef5e1461aa5ad1b05c5801159
Sma3in96 <oumouss.ismail@gmail.com>
2023-12-15 13:19:28 +0100

    task5

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa starts with "N"
+List all cities from a database
 """
 import sys
 import MySQLdb
@@ -11,8 +11,8 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.states_id = states.id;")
-    result = cur.fetchall()
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = cur.fetchall()
 
-    for i in result:
-        print(i)
+    for state in states:
+        print(state)
