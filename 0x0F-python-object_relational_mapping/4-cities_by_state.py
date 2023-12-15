143ddff9c9be85ce0b8f40bbd217202132dd9323
Sma3in96 <oumouss.ismail@gmail.com>
2023-12-15 13:01:43 +0100

    task 2

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa starts with "N"
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+cur = db.cursor()
+cur.execute("SELECT cities.id cities.name states.name \
+FROM cities JOIN states ON cities.states_id = states.id;")
+result = cur.fetchall()
+
+for i in result:
+    print(i)
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa starts with "N"
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.name \
+    FROM cities JOIN states ON cities.states_id = states.id \
+    WHERE states.name = %s;", (sys.argv[4]))
+    result = cur.fetchall()
+
+    print(", ".join([lis[0] for lis in result]))
