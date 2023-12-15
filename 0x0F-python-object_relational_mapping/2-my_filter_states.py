3132dbfc6e6ad625de011572617501e3d96219b0
Sma3in96 <oumouss.ismail@gmail.com>
2023-12-15 12:21:00 +0100

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,7 @@ if __name__ == '__main__':
     cur.execute("SELECT * \
     FROM states \
     WHERE CONVERT(`name` USING latin1) \
-    COLLATE Latin1_General_CS \
-    = '{}';").format(sys.argv[4])
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
 
     for state in states:
