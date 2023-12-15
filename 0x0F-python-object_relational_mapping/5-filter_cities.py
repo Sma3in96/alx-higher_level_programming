b386d3078429e43fea21690f64c61d5fb5d7aae2
Sma3in96 <oumouss.ismail@gmail.com>
2023-12-15 13:18:21 +0100

    task5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa starts with "N"
+List all cities of a state
 """
 import sys
 import MySQLdb
@@ -8,9 +8,11 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
+
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.states_id = states.id \
+    FROM cities JOIN states ON cities.state_id = states.id \
     WHERE states.name = '{}';".format(sys.argv[4]))
-    result = cur.fetchall()
-    print(", ".join([lis[0] for lis in result]))
+    states = cur.fetchall()
+
+    print(", ".join([state[1] for state in states]))
