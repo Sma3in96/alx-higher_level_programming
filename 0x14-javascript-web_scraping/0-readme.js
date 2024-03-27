c6a9658eb940ceae32908b50d5d5924a1be97a3b
Sma3in96 <oumouss.ismail@gmail.com>
2024-03-27 00:19:27 +0000

    text

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const args = process.argv;
 
 fs.readFile(args[2], 'utf8', (error, data) => {
-    if (error) {
-        console.error(error);
-        return
-    }
-    console.log(data)
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(data);
 });
