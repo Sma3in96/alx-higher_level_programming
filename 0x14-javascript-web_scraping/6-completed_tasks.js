ff241ef8c33f34853726ecdb162d876b2913ca8e
Sma3in96 <oumouss.ismail@gmail.com>
2024-03-27 03:02:07 +0000

    task6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+  } else {
+    const tasks = JSON.parse(body);
+    const dict = {}
+    for (let i = 1; i <= 10; i++) {
+        dict[i] = 0
+    }
+    for (const task in tasks) {
+        if (tasks[task].completed === true) {
+            let index = tasks[task].userId
+            dict[index]++;
+        }
+    }
+    console.log(dict)
+  }
+});
