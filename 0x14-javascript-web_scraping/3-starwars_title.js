0629e4c77a15cbeb7bc98a32b146924594252bcd
Sma3in96 <oumouss.ismail@gmail.com>
2024-03-27 01:51:15 +0000

    task3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
